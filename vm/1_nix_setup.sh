d93b33176212cce379247c96ddcc987adaa8c6b9
fmaurer <fmaurer@school>
2025-10-08 17:04:39 +0200

    add more 1_nix_setup

--- vm/1_nix_setup.sh
+++ vm/1_nix_setup.sh
@@ -4,19 +4,21 @@
 # the only that needs to be done in vm window:
 passwd root # -> set simple pw
 
+ssh-keygen -f "/home/fmaurer/.ssh/known_hosts" -R "[localhost]:5555"; ssh -p 5555 root@localhost
 
-# one-liner for creating partition
-parted /dev/sda mklabel msdos mkpart primary ext4 1MiB 100% set 1 boot on
+## one-liner for creating partition
+# parted /dev/sda mklabel msdos mkpart primary ext4 1MiB 100% set 1 boot on
 
-# one-liner for formatting etc.
-mkfs.ext4 /dev/sda1 && mount /dev/sda1 /mnt && nixos-generate-config --root /mnt
+## one-liner for formatting etc.
+# mkfs.ext4 /dev/sda1 && mount /dev/sda1 /mnt && nixos-generate-config --root /mnt
 
 # 1st all-in-one-liner:
 parted -s /dev/sda mklabel msdos mkpart primary ext4 1MiB 100% set 1 boot on && \
 mkfs.ext4 /dev/sda1 && mount /dev/sda1 /mnt && nixos-generate-config --root /mnt
 
-
+# rsync configuration.nix to vm
 rsync -vaz ./vm-conf.nix -e "ssh -p 5555" root@localhost:/mnt/etc/nixos/configuration.nix
-# copy over configuration.nix
+
+# then on vm
 nixos-install
-reboot
+shutdown -h now
