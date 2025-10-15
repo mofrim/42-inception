bf1494ad31fe60a3e6ac0e88d817033ddc3ace2c
mofrim <mofrim@posteo.de>
2025-10-15 17:03:26 +0200

    add new tools funcs

--- tools/tools_include.sh
+++ tools/tools_include.sh
@@ -11,3 +11,34 @@ function logmsg () {
 function escape_sed() {
   echo "$1" | sed 's/[][}{}^/()$&.*+?|]/\\&/g'
 }
+
+# print every line starting with a '#' in green
+function print_cmds_green() {
+  local file="$1"
+  while IFS= read -r line; do
+    if [[ "$line" =~ ^\# ]]; then
+      # Print in grey
+    echo "$line"
+  else
+    # Print in green
+    echo -e "\e[32m$line\e[0m"
+    fi
+  done < "$file"
+}
+
+ask_yes_no() {
+  local prompt="$1"
+  local default="${2:-n}"
+  local answer
+
+  while true; do
+    read -r -p "$prompt [y/n] ($default): " answer
+    answer="${answer:-$default}"
+
+    case "$answer" in
+      [Yy]*) return 0 ;;
+      [Nn]*) return 1 ;;
+      *) echo "Please answer y or n." ;;
+    esac
+  done
+}
