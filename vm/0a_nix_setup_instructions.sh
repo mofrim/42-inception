7a129558f89a9bdc43bc0537460d3c0874759198
fmaurer <fmaurer@school>
2026-03-13 12:24:05 +0100

    remove more garbage

--- vm/0a_nix_setup_instructions.sh
+++
@@ -1,8 +0,0 @@
-## (1) the only thing to be done in vm window: set a very simple pw for root
-sudo passwd root
-
-## (2) run setup_vm_system (alias for './0a_vm_setup_oneliner.sh')
-setup_vm_system
-
-## (3) leave vm-install-shell again using ctrl-d or
-exit
