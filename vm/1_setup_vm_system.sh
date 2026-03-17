f31fa6b383df3d393ad24e7dc79913c4f9a6e7c8
fmaurer <fmaurer@school>
2026-03-17 11:36:30 +0100

    vm: cleanup setup script &nixos-install more lines

--- vm/1_setup_vm_system.sh
+++ vm/1_setup_vm_system.sh
@@ -20,24 +20,6 @@ fi
 
 logmsg "doing (most) setup on VM and installing system..."
 
-# function vmsetup() {
-# 	ssh-keygen -f "/home/$(id -un)/.ssh/known_hosts" -R "[localhost]:5555" 
-# 	ssh -q -o StrictHostKeyChecking=no $SSH_KEYOPT -p 5555 root@localhost "parted -s /dev/sda mklabel msdos mkpart primary ext4 1MiB 100% set 1 boot on && mkfs.ext4 /dev/sda1  && mount /dev/sda1 /mnt && nixos-generate-config --root /mnt " 
-# 	rsync -vaz ./vm-conf.nix -e "ssh $SSH_KEYOPT -p 5555" root@localhost:/mnt/etc/nixos/configuration.nix 
-#
-# 	## run nixos-install in case the image does not bring along nixpkgs package
-# 	## source.. this leads to another download step during install
-# 	#
-# 	# ssh -q -o StrictHostKeyChecking=no -p 5555 root@localhost "nix-channel -vv --update && nixos-install --no-root-password && shutdown -h now"
-#
-# 	## nixos-install without downloading nixpkgs. requires nixpkgs sources to be present inside image.
-# 	#
-# 	ssh -q -o StrictHostKeyChecking=no $SSH_KEYOPT -p 5555 root@localhost "nixos-install --no-root-password && shutdown -h now"
-# }
-#
-# # TODO: make this docker limited like in docker-cli output
-# $INCEP_TOOLDIR/limitOut42/limitOut42 42< <(vmsetup)
-
 limitOut42="$INCEP_TOOLDIR/limitOut42/limitOut42"
 sshCmd="ssh -q -o StrictHostKeyChecking=no $SSH_KEYOPT -p 5555 root@localhost"
 
@@ -64,7 +46,6 @@ sleep 1
 logmsg ' "nixos-generate-config --root /mnt"'
 $limitOut42 42< <($sshCmd "nixos-generate-config --root /mnt")
 
-
 sleep 1
 
 logmsg 'rsync -vaz ./vm-conf.nix -e "ssh $SSH_KEYOPT -p 5555" root@localhost:/mnt/etc/nixos/configuration.nix'
@@ -74,7 +55,7 @@ sleep 1
 
 logmsg 'ssh -q -o StrictHostKeyChecking=no $SSH_KEYOPT -p 5555 root@localhost "nixos-install --no-root-password"'
 
-$limitOut42 8 10000 42< <($sshCmd 'nixos-install --no-root-password 2>&1')
+$limitOut42 14 10000 42< <($sshCmd 'nixos-install --no-root-password 2>&1')
 
 sleep 1
 
